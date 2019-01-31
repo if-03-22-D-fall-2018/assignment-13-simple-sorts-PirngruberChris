cb1f068987ddc3993fbc69f926372c82b96b573a
PirngruberChris <christianpirngruber3@gmail.com>
2019-01-31 13:34:22 +0100

    started h files made

--- sorting_algorithms.cpp
+++ sorting_algorithms.cpp
@@ -10,3 +10,19 @@
  * <your description here>
  *-----------------------------------------------------------------------------
 */
+#include "sorting_algorithms.h"
+#include <time.h>
+#include <stdlib.h>
+
+void init_random(int *array, unsigned long length)
+{
+	srandom(time(NULL));
+	for (unsigned long i = 0; i < length; i++)
+		array[i] = random();
+}
+void bubble_sort(int *array, const int size){
+
+}
+void insertion_sort(int *array, const int size){
+
+}
--- sorting_algorithms.h
+++ sorting_algorithms.h
@@ -13,6 +13,10 @@
 #ifndef ___SORTING_ALGORITHMS_H
 #define ___SORTING_ALGORITHMS_H
 
+
+void init_random(int *array, unsigned long length);
+void bubble_sort(int *array, unsigned long length);
+void insertion_sort(int *array, unsigned long length);
 /**
 *** @file sorting_algorithms.h
 *** @brief Description of sorting algorithms
@@ -20,6 +24,4 @@
 *** Basically two functions to sort arrays of integer elements. Furthermore, a function
 *** to initialize an array with a number of random elements.
 */
-
-
 #endif
